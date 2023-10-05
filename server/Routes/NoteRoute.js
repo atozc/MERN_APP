bac0470f3a2b787e2ace5c6c7210b4a18e53fcbe
atozc <catoz@nevada.unr.edu>
2023-10-05 16:28:38 -0700

    Update NoteRoute.js

--- server/Routes/NoteRoute.js
+++ server/Routes/NoteRoute.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { getAllUserNotes,createNote,getNote,updateNote,deleteNote } = require("../Controllers/NoteController");
+const { getAllUserNotes, createNote, getNote, updateNote, deleteNote } = require("../Controllers/NoteController");
+const { userVerification } = require('../Middlewares/AuthMiddleware')
 
 // CRUD Operations
 
 // Get notes by providing userId
-router.route('/:userId').get(getAllUserNotes);
+router.route('/:userId').get(userVerification, getAllUserNotes);
 
 // Create note
 router.route('/create/:userId').post(createNote);
